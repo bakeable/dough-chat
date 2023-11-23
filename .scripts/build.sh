0b64a8154e4047fbc8e72e83e7fb02019acd702b
bakeable <robin@bakeable.nl>
2023-11-23 19:37:50 -0300

    Initial commit

---
+++ .scripts/build.sh
@@ -0,0 +1,2 @@
+docker build -t bakeable/dough-chat
+docker run -p 3000:3000 -d bakeable/dough-chat
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { Server as SocketIOServer } from 'socket.io';
+import http from 'http';
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIOServer(server);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+io.on('connection', (socket: any) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
