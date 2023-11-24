56d4c62457244c88f2a80ecb6bcf02fe1feeee3c
bakeable <robin@bakeable.nl>
2023-11-23 21:26:07 -0300

    Added Vue Client

--- build/index.js
+++ build/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { Server as SocketIOServer } from 'socket.io';
 import http from 'http';
+import { fileURLToPath } from 'url';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer(server);
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '/views/client.html'));
 });
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -14,5 +18,5 @@ io.on('connection', (socket) => {
     });
 });
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+    console.log('Listening on https://localhost:3000');
 });
